534153d2af0309143678c47e21e1c0d3473b16a2
Murray King <murray.king@ed.ac.uk>
2014-12-15 16:53:19 +0000

    change to fragment

--- app/src/main/java/com/example/murray/testapp/MainMapView.java
+++ app/src/main/java/com/example/murray/testapp/MainMapView.java
@@ -1,243 +1,42 @@
 package com.example.murray.testapp;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-
-import org.osmdroid.DefaultResourceProxyImpl;
-import org.osmdroid.ResourceProxy;
-import org.osmdroid.bonuspack.kml.KmlDocument;
-import org.osmdroid.bonuspack.overlays.FolderOverlay;
-import org.osmdroid.tileprovider.MapTileProviderArray;
-import org.osmdroid.tileprovider.modules.IArchiveFile;
-import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
-import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
-import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
-import org.osmdroid.tileprovider.tilesource.XYTileSource;
-import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
-import org.osmdroid.util.BoundingBoxE6;
-import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-import java.io.File;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by murray on 25/08/14.
  */
-public class MainMapView extends Activity {
+public class MainMapView extends FragmentActivity {
 
-    private SharedPreferences prefs;
-    public static final String PREFS_NAME = "com.example.murray.testapp.prefs";
-    public static final String PREFS_SCROLL_X = "scrollX";
-    public static final String PREFS_SCROLL_Y = "scrollY";
-    public static final String PREFS_ZOOM_LEVEL = "zoomLevel";
-    public static final String PREFS_SHOW_LOCATION = "showLocation";
-    public static final String PREFS_SHOW_COMPASS = "showCompass";
-    MyLocationNewOverlay locationOverlay;
-    private CompassOverlay compassOverlay;
-
-    KmlDocument kmlDocument;
-    FixedMapView mapView;
-    Utils utils = Utils.getInstance();
+    private static final String MAP_FRAGMENT_TAG = "com.example.murray.testapp.MAP_FRAGMENT_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Context context = this.getApplicationContext();
-        prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        SingleRow routeRow = (SingleRow)getIntent().getSerializableExtra(MyActivity.ROUTE_CHOSEN_KEY);
+        this.setContentView(R.layout.main);
 
 
-        kmlDocument = new KmlDocument();
+        FragmentManager fm = this.getSupportFragmentManager();
 
-
-
-
-
-
-        File route = utils.copyFileFromAssets(routeRow.getRouteKmlFile(), this.getAssets(), this.getPackageName());
-
-        boolean success = kmlDocument.parseKMLFile(route);
-
-        // Tell the activity which XML layout is right
-        setContentView(R.layout.map_view);
-        /**
-         * This whole thing revolves around instantiating a MainMapView class, way,
-         * way below. And MainMapView requires a ResourceProxy. Who are we to deny
-         * its needs? Let's create one!
-         *
-         * It would have been nice if this was taken care of in the MainMapView
-         * constructor. Interestingly MainMapView *has* a constructor that creates a
-         * new DefaultResourceProxyImpl but unfortunately that one doesn't allow
-         * us to specify the parameters we *do* need to set ...
-         */
-        DefaultResourceProxyImpl resProxy;
-        resProxy = new DefaultResourceProxyImpl(this.getApplicationContext());
-
-        /**
-         * A class that implements the ITileSource interface knows how to
-         * convert an InputStream or a file path into a Drawable. It doesn't do
-         * much more than that. The real 'sourcery' is performed by
-         * MapTileFileArchiveProvider which will be introduced shortly.
-         *
-         * What we need is really a BitmapTileSourceBase instance, but this
-         * class is defined as abstract. XYTileSource is not and comes closest
-         * to what we want.
-         *
-         * Comment: I don't quite get why BitmapTileSource base is abstract; it
-         * doesn't contain any abstract methods.
-         */
-
-        XYTileSource tSource;
-        tSource = new XYTileSource("mbtiles",
-                ResourceProxy.string.offline_mode,
-                8, 19, 256, ".png", new String[]{"http://who.cares/"});
-
-
-
-        IArchiveFile[] files = { MBTilesFileArchive.getDatabaseFileArchive(utils.getOfflineMap()) };
-        SimpleRegisterReceiver sr = new SimpleRegisterReceiver(this);
-
-        MapTileModuleProviderBase moduleProvider;
-        moduleProvider = new MapTileFileArchiveProvider(sr, tSource, files);
-
-        MapTileModuleProviderBase[] pBaseArray;
-        pBaseArray = new MapTileModuleProviderBase[] { moduleProvider };
-
-        MapTileProviderArray provider;
-        provider = new MapTileProviderArray(tSource, null, pBaseArray);
-
-        mapView = new FixedMapView(this, 256, resProxy, provider);
-        mapView.setBuiltInZoomControls(true);
-        this.locationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context),
-                mapView);
-
-        Button myUselessButton = new Button(this);
-
-        myUselessButton.setText("Click");
-
-
-
-        final RelativeLayout relativeLayout = new RelativeLayout(this);
-
-        final RelativeLayout.LayoutParams mapViewLayoutParams = new RelativeLayout.LayoutParams(
-
-                RelativeLayout.LayoutParams.FILL_PARENT,RelativeLayout.LayoutParams.FILL_PARENT);
-
-        final RelativeLayout.LayoutParams textViewLayoutParams = new RelativeLayout.LayoutParams(
-
-                RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        final RelativeLayout.LayoutParams buttonLayoutParams = new RelativeLayout.LayoutParams(
-
-                RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        buttonLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-
-
-
-        relativeLayout.addView(mapView, mapViewLayoutParams);
-
-
-        relativeLayout.addView(myUselessButton,buttonLayoutParams);
-
-        setContentView(relativeLayout);
-
-        final BoundingBoxE6 bb =  kmlDocument.mKmlRoot.getBoundingBox();
-
-        myUselessButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mapView.getController().setCenter(bb.getCenter());
-            }
-        });
-
-        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(this);
-        this.mapView.getOverlays().add(scaleBarOverlay);
-
-
-        FolderOverlay kmlOverlay = (FolderOverlay)kmlDocument.mKmlRoot.buildOverlay( mapView, null, null, kmlDocument);
-
-        mapView.getOverlays().add(kmlOverlay);
-        mapView.getOverlays().add(this.locationOverlay);
-        mapView.setClickable(true);
-
-        mapView.setMultiTouchControls(true);
-
-
-        //controller.zoomToSpan(boundingBoxE6.getLatitudeSpanE6(), boundingBoxE6.getLongitudeSpanE6());
-
-        // Set the MainMapView as the root View for this Activity; done!
-        setContentView(relativeLayout);
-        mapView.getController().setZoom(15); //set initial zoom-level, depends on your need
-
-
-        final ViewTreeObserver vto = mapView.getViewTreeObserver();
-        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-
-
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                else
-                    mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-
-                mapView.getController().animateTo(bb.getCenter());
-            }
-        });
-
-        //controller.zoomToSpan(boundingBoxE6.getLatitudeSpanE6(),boundingBoxE6.getLongitudeSpanE6());
-        // Enable the "Up" button for more navigation options
+        if (fm.findFragmentByTag(MAP_FRAGMENT_TAG) == null) {
+            MapFragment mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_container, mapFragment, MAP_FRAGMENT_TAG).commit();
+        }
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        this.compassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
-                mapView);
-        mapView.getController().setZoom(prefs.getInt(PREFS_ZOOM_LEVEL, 1));
-        mapView.scrollTo(prefs.getInt(PREFS_SCROLL_X, 0), prefs.getInt(PREFS_SCROLL_Y, 0));
-
-        locationOverlay.enableMyLocation();
-        compassOverlay.enableCompass();
     }
-
-
 
     @Override
-    public void onPause()
-    {
-        final SharedPreferences.Editor edit = prefs.edit();
-        edit.putInt(PREFS_SCROLL_X, mapView.getScrollX());
-        edit.putInt(PREFS_SCROLL_Y, mapView.getScrollY());
-        edit.putInt(PREFS_ZOOM_LEVEL,  mapView.getZoomLevel());
-        edit.putBoolean(PREFS_SHOW_LOCATION, locationOverlay.isMyLocationEnabled());
-        edit.putBoolean(PREFS_SHOW_COMPASS, compassOverlay.isCompassEnabled());
-        edit.commit();
-
-        this.locationOverlay.disableMyLocation();
-        this.compassOverlay.disableCompass();
-
-        super.onPause();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-    @Override
-    public void  onResume() {
-        super.onResume();
 
-        if (prefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
-            this.locationOverlay.enableMyLocation();
-        }
-        if (prefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
-            this.compassOverlay.enableCompass();
-        }
-    }
+
 }
---
+++ app/src/main/java/com/example/murray/testapp/MapFragment.java
@@ -0,0 +1,209 @@
+package com.example.murray.testapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.bonuspack.kml.KmlDocument;
+import org.osmdroid.bonuspack.overlays.FolderOverlay;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.modules.IArchiveFile;
+import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.util.BoundingBoxE6;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.File;
+
+/**
+ * Created by murrayking on 15/12/2014.
+ */
+public class MapFragment extends Fragment {
+
+
+    private SharedPreferences prefs;
+    public static final String PREFS_NAME = "com.example.murray.testapp.prefs";
+    public static final String PREFS_SCROLL_X = "scrollX";
+    public static final String PREFS_SCROLL_Y = "scrollY";
+    public static final String PREFS_ZOOM_LEVEL = "zoomLevel";
+    public static final String PREFS_SHOW_LOCATION = "showLocation";
+    public static final String PREFS_SHOW_COMPASS = "showCompass";
+    private MyLocationNewOverlay locationOverlay;
+    private CompassOverlay compassOverlay;
+
+    KmlDocument kmlDocument;
+    FixedMapView mapView;
+    Utils utils = Utils.getInstance();
+
+
+    public static MapFragment newInstance() {
+        MapFragment fragment = new MapFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        SingleRow routeRow = (SingleRow)getActivity().getIntent().getSerializableExtra(MyActivity.ROUTE_CHOSEN_KEY);
+
+        kmlDocument = new KmlDocument();
+
+        File route = utils.copyFileFromAssets(routeRow.getRouteKmlFile(), this.getActivity().getAssets(), this.getActivity().getPackageName());
+
+        kmlDocument.parseKMLFile(route);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Context context = inflater.getContext();
+
+        DefaultResourceProxyImpl resProxy;
+        resProxy = new DefaultResourceProxyImpl(context);
+
+        /**
+         * A class that implements the ITileSource interface knows how to
+         * convert an InputStream or a file path into a Drawable. It doesn't do
+         * much more than that. The real 'sourcery' is performed by
+         * MapTileFileArchiveProvider which will be introduced shortly.
+         *
+         * What we need is really a BitmapTileSourceBase instance, but this
+         * class is defined as abstract. XYTileSource is not and comes closest
+         * to what we want.
+         *
+         * Comment: I don't quite get why BitmapTileSource base is abstract; it
+         * doesn't contain any abstract methods.
+         */
+
+        XYTileSource tSource;
+        tSource = new XYTileSource("mbtiles",
+                ResourceProxy.string.offline_mode,
+                8, 19, 256, ".png", new String[]{"http://who.cares/"});
+
+
+        IArchiveFile[] files = { MBTilesFileArchive.getDatabaseFileArchive(utils.getOfflineMap()) };
+        SimpleRegisterReceiver sr = new SimpleRegisterReceiver(context);
+
+        MapTileModuleProviderBase moduleProvider;
+        moduleProvider = new MapTileFileArchiveProvider(sr, tSource, files);
+
+        MapTileModuleProviderBase[] pBaseArray;
+        pBaseArray = new MapTileModuleProviderBase[] { moduleProvider };
+
+        MapTileProviderArray provider;
+        provider = new MapTileProviderArray(tSource, null, pBaseArray);
+
+        mapView = new FixedMapView(context, 256, resProxy, provider);
+
+
+        return mapView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button mButton = (Button) getActivity().findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // here you set what you want to do when user clicks your button,
+                // e.g. launch a new activity
+                MapFragment.this.locationOverlay.enableFollowLocation();
+            }
+        });
+
+        Context context = this.getActivity();
+        prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        mapView.setBuiltInZoomControls(true);
+        this.locationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context), mapView);
+
+
+        final BoundingBoxE6 bb =  kmlDocument.mKmlRoot.getBoundingBox();
+
+        FolderOverlay kmlOverlay = (FolderOverlay)kmlDocument.mKmlRoot.buildOverlay(mapView, null, null, kmlDocument);
+
+        mapView.getOverlays().add(kmlOverlay);
+        mapView.getOverlays().add(this.locationOverlay);
+        mapView.setClickable(true);
+
+        mapView.setMultiTouchControls(true);
+
+
+        mapView.getController().setZoom(15); //set initial zoom-level, depends on your need
+
+
+        final ViewTreeObserver vto = mapView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                    mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                else
+                    mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+
+                mapView.getController().animateTo(bb.getCenter());
+            }
+        });
+
+
+
+        this.compassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
+                mapView);
+        mapView.getController().setZoom(prefs.getInt(PREFS_ZOOM_LEVEL, 1));
+        mapView.scrollTo(prefs.getInt(PREFS_SCROLL_X, 0), prefs.getInt(PREFS_SCROLL_Y, 0));
+
+        locationOverlay.enableMyLocation();
+        compassOverlay.enableCompass();
+    }
+
+
+
+    @Override
+    public void onPause()
+    {
+        final SharedPreferences.Editor edit = prefs.edit();
+        edit.putInt(PREFS_SCROLL_X, mapView.getScrollX());
+        edit.putInt(PREFS_SCROLL_Y, mapView.getScrollY());
+        edit.putInt(PREFS_ZOOM_LEVEL,  mapView.getZoomLevel());
+        edit.putBoolean(PREFS_SHOW_LOCATION, locationOverlay.isMyLocationEnabled());
+        edit.putBoolean(PREFS_SHOW_COMPASS, compassOverlay.isCompassEnabled());
+        edit.commit();
+
+        this.locationOverlay.disableMyLocation();
+        this.compassOverlay.disableCompass();
+
+        super.onPause();
+    }
+    @Override
+    public void  onResume() {
+        super.onResume();
+
+        if (prefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
+            this.locationOverlay.enableMyLocation();
+        }
+        if (prefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
+            this.compassOverlay.enableCompass();
+        }
+    }
+}
